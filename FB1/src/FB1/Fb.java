9bcd58400d01e6c10f680a241bce3dcfa4873872
amishaaggarwal <amishaaggarwal10@gmail.com>
2020-10-28 15:18:13 +0530

    Final test cases and conditions added

--- FB1/src/FB1/Fb.java
+++ FB1/src/FB1/Fb.java
@@ -4,14 +4,19 @@ public class Fb {
 
 	public String action(int i) 
 	{
-		if(i%15==0)
-			return "FizzBuzz";
-		else if(i%3==0)
-			return "Fizz";
-		else if(i%5==0)
-			return "Buzz";
+		if(i<=100 && i>=1)
+		{
+			if(i%15==0)
+				return "FizzBuzz";
+			else if(i%3==0)
+				return "Fizz";
+			else if(i%5==0)
+				return "Buzz";
+			else
+				return i+"";
+		}
 		else
-			return null;
+			return "Number entered is not in range of 1-100";
 	}
 
 }
---
+++ FB1/src/FB1/Test4.java
@@ -0,0 +1,16 @@
+package FB1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test4 {
+	@Test
+	public void test()
+	{
+		Fb fizzbuzz=new Fb();
+		Assert.assertEquals("Number entered is not in range of 1-100",fizzbuzz.action(130));
+		Assert.assertEquals("1",fizzbuzz.action(1));
+		Assert.assertEquals("Number entered is not in range of 1-100",fizzbuzz.action(0));
+	}
+
+}
