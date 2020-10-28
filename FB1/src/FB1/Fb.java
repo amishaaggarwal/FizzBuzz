76ac8901947330dd34d101dcdf604f1c80877bf0
amishaaggarwal <amishaaggarwal10@gmail.com>
2020-10-28 15:05:41 +0530

    Added test and condition for Fizz

---
+++ FB1/src/FB1/Fb.java
@@ -0,0 +1,13 @@
+package FB1;
+
+public class Fb {
+
+	public String action(int i) 
+	{
+		if(i%3==0)
+			return "Fizz";
+		else 
+			return null;
+	}
+
+}
--- FB1/src/FB1/Test1.java
+++ FB1/src/FB1/Test1.java
@@ -1,5 +1,14 @@
 package FB1;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Test1 {
+	@Test
+	public void test()
+	{
+		Fb fizzbuzz=new Fb();
+		Assert.assertEquals("Fizz",fizzbuzz.action(21));
+	}
 
 }
